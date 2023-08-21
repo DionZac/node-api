70586740b73ca6ab5052497eac62c1027247fd5f
DionZac <zacdion333@gmail.com>
2023-08-21 15:28:43 +0300

    Update migration script

--- migrations/0001_add_field_account_uid.js
+++ migrations/0001_add_field_account_uid.js
@@ -1,16 +1,5 @@
     exports.migration = {
-        operations:[
-{
-    "type": "add_field",
-    "dbname": "live",
-    "field": {
-        "fname": "account_uid",
-        "type": "str",
-        "len": 160,
-        "def": "91w4brrj7r",
-        "hlp": "Account UUID"
-    }
-},{
+        operations:[{
     "type": "add_field",
     "dbname": "singles",
     "field": {
